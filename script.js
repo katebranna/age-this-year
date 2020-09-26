fa8fc94c47ff8ed39589effeb933b9009c75b359
Kate Branna <70902635+katebranna@users.noreply.github.com>
2020-09-26 13:11:18 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function age() {
-var birthYear = prompt("What year were you born?");
-var currentTime = new Date();
-var thisYear = currentTime.getFullYear();
-var age = (thisYear - birthYear);
-var h1 = document.createElement('h1');
-var textAnswer = document.createTextNode("You are turning " + age + "  this year");
+let birthYear = prompt("What year were you born?");
+let currentTime = new Date();
+let thisYear = currentTime.getFullYear();
+let age = (thisYear - birthYear);
+let h1 = document.createElement('h1');
+let textAnswer = document.createTextNode("You are turning " + age + "  this year");
 h1.setAttribute('id', 'age');
 h1.appendChild(textAnswer);
 document.getElementById('flex-box-result').appendChild(h1);
